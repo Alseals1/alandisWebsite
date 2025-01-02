356a18057722af8b82abb30bc6c475c3292abef8
Alandis <1seals.alandis@gmail.com>
2025-01-01 21:17:46 -0600

    Modified file spelling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import { Hero } from "./components/hero/Hero";
 import About from "./components/about/About";
-import Parallax from "./components/parallax/parallax";
+import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
